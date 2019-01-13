365853bda2ae65451a6dc5c431f47869b9057416
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-01-14 00:12:47 +0900

    feat: HTTP Proxyの転送先を HOSTNAME:PORT の設定機能を追加
    環境ごとに異なるため設定できないと不便
    環境変数から渡す

--- http-proxy-server/start-proxy.sh
+++ http-proxy-server/start-proxy.sh
@@ -7,7 +7,7 @@ echo ========================
 cat << EOS | tee ./squid.conf
 access_log stdio:/dev/stdout common
 
-cache_peer 10.0.2.2 parent 8080 0
+cache_peer ${PROXY_FORWARDING_HOSTNAME} parent ${PROXY_FORWARDING_PORT} 0
 never_direct allow all
 
 http_access allow all
