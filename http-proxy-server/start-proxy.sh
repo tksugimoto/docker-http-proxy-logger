6e5663599b620139fd1a5a4e23b35019203bfc45
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-02-14 00:42:12 +0900

    feat: parent proxy を削除
    TODO: 設定でparent proxyの有無を切り替えできるようにする

--- http-proxy-server/start-proxy.sh
+++ http-proxy-server/start-proxy.sh
@@ -7,9 +7,6 @@ echo ========================
 cat << EOS | tee ./squid.conf
 access_log stdio:/dev/stdout common
 
-cache_peer ${PROXY_FORWARDING_HOSTNAME} parent ${PROXY_FORWARDING_PORT} 0
-never_direct allow all
-
 http_access allow all
 
 http_port 3128
@@ -25,7 +22,6 @@ echo ========================
 
 echo
 echo Proxy started
-echo "  Parent proxy: http://${PROXY_FORWARDING_HOSTNAME}:${PROXY_FORWARDING_PORT}/"
 
 # -N: No daemon mode.
 exec squid -f ./squid.conf -N
