9f2eedbe941970fc3315b3dfc72c1e0cc523f09b
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-09-30 00:11:42 +0900

    Revert "feat: parent proxy を削除"
    This reverts commit 6e5663599b620139fd1a5a4e23b35019203bfc45.

--- http-proxy-server/start-proxy.sh
+++ http-proxy-server/start-proxy.sh
@@ -7,6 +7,9 @@ echo ========================
 cat << EOS | tee ./squid.conf
 access_log stdio:/dev/stdout common
 
+cache_peer ${PROXY_FORWARDING_HOSTNAME} parent ${PROXY_FORWARDING_PORT} 0
+never_direct allow all
+
 http_access allow all
 
 http_port 3128
@@ -22,6 +25,7 @@ echo ========================
 
 echo
 echo Proxy started
+echo "  Parent proxy: http://${PROXY_FORWARDING_HOSTNAME}:${PROXY_FORWARDING_PORT}/"
 
 # -N: No daemon mode.
 exec squid -f ./squid.conf -N
