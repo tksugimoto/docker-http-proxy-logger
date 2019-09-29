3e27636a910d5ba14d90ff96f7d95408bff36401
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-09-30 01:30:57 +0900

    feat: parent proxy を option 化
    親proxy関連の環境変数が未設定の場合 DIRECT モード
    - PROXY_FORWARDING_HOSTNAME
    - PROXY_FORWARDING_PORT

--- http-proxy-server/start-proxy.sh
+++ http-proxy-server/start-proxy.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -e
 
+if [ "$PROXY_FORWARDING_HOSTNAME" = "" ] || [ "$PROXY_FORWARDING_PORT" = "" ]; then
+	readonly is_direct=true
+fi
+
 echo Generate squid.conf
 echo ========================
 
 cat << EOS | tee ./squid.conf
 access_log stdio:/dev/stdout common
-
-cache_peer ${PROXY_FORWARDING_HOSTNAME} parent ${PROXY_FORWARDING_PORT} 0
-never_direct allow all
 
 http_access allow all
 
@@ -21,11 +22,24 @@ pid_filename none
 shutdown_lifetime 1 seconds
 EOS
 
+if [ "$is_direct" != "true" ]; then
+	cat <<- EOS | tee -a ./squid.conf
+
+		cache_peer ${PROXY_FORWARDING_HOSTNAME} parent ${PROXY_FORWARDING_PORT} 0
+		never_direct allow all
+	EOS
+fi
+
 echo ========================
 
 echo
 echo Proxy started
-echo "  Parent proxy: http://${PROXY_FORWARDING_HOSTNAME}:${PROXY_FORWARDING_PORT}/"
+
+if [ "$is_direct" = "true" ]; then
+	echo "  Parent proxy: none"
+else
+	echo "  Parent proxy: http://${PROXY_FORWARDING_HOSTNAME}:${PROXY_FORWARDING_PORT}/"
+fi
 
 # -N: No daemon mode.
 exec squid -f ./squid.conf -N
