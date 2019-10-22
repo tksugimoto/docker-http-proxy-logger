3707a6a733f8e1a3b5140a202b672e21fdd5a840
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-10-22 13:22:25 +0900

    docs: ログ書式の説明を追加

--- http-proxy-server/start-proxy.sh
+++ http-proxy-server/start-proxy.sh
@@ -16,6 +16,15 @@ echo Generate squid.conf
 echo ========================
 
 cat <<- EOS | tee ./squid.conf
+	# logformat
+	# tl: Local time
+	# >a: Client source IP address
+	# un: User name (any available)
+	# >Hs: HTTP status code sent to the client
+	# rm: Request method (GET/POST etc)
+	# ru: Request URL received (or computed) and sanitized
+	# rv: Request protocol version
+	# >h: Original received request header
 	logformat custom   [%{%Y/%m/%d %H:%M:%S %z}tl] %>a %[un [%>Hs] "%rm %ru HTTP/%rv" "%{User-Agent}>h"
 	access_log stdio:/dev/stdout custom
 
